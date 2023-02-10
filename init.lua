3233adb96c0be555e675c499ecabfb15bc25e69f
Angelo Zapata <angelozapata@inf.udec.cl>
2023-02-10 16:24:11 -0300

    [+] Support for linux.

--- init.lua
+++ init.lua
@@ -10,10 +10,11 @@ local has = function(x)
 end
 
 local is_mac = has("macunix")
+local is_linux = has("linux")
 local is_win = has("win32")
 
-if is_mac then
-	require("macos")
+if is_mac or is_linux then
+	require("unix")
 end
 if is_win then
 	require("windows")
