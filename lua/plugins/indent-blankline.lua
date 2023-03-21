c12675e7d8b62b7006f2348ff869d11e1c1f5c03
Angelo Zapata <angelozapata@inf.udec.cl>
2023-03-20 21:06:28 -0300

    [c] indent blankline

--- lua/plugins/indent-blankline.lua
+++ lua/plugins/indent-blankline.lua
@@ -5,7 +5,7 @@ return {
 		buftype_exclude = { "terminal" },
 		show_trailing_blankline_indent = false,
 		show_first_indent_level = false,
-		show_current_context = true,
-		show_current_context_start = true,
+		show_current_context = false,
+		show_current_context_start = false,
 	},
 }
