01e7b529e21f5eb227d1ca6a3cda9eb99e0f131f
Angelo Zapata <angelozapata@inf.udec.cl>
2023-04-10 19:00:51 -0400

    [c] telescope size

--- lua/sixzen/telescope/setup.lua
+++ lua/sixzen/telescope/setup.lua
@@ -18,31 +18,31 @@ require("telescope").setup {
     selection_caret = "❯ ",
     winblend = 0,
     layout_strategy = "horizontal",
-    -- layout_config = {
-    --   width = 0.95,
-    --   height = 0.85,
-    --   -- preview_cutoff = 120,
-    --   prompt_position = "bottom",
-    --   horizontal = {
-    --     preview_width = function(_, cols, _)
-    --       if cols > 200 then
-    --         return math.floor(cols * 0.5)
-    --       else
-    --         return math.floor(cols * 0.6)
-    --       end
-    --     end,
-    --   },
-    --   vertical = {
-    --     width = 0.9,
-    --     height = 0.95,
-    --     preview_height = 0.5,
-    --   },
-    --   flex = {
-    --     horizontal = {
-    --       preview_width = 0.9,
-    --     },
-    --   },
-    -- },
+    layout_config = {
+      width = 0.95,
+      height = 0.85,
+      -- preview_cutoff = 120,
+      prompt_position = "bottom",
+      horizontal = {
+        preview_width = function(_, cols, _)
+          if cols > 200 then
+            return math.floor(cols * 0.5)
+          else
+            return math.floor(cols * 0.6)
+          end
+        end,
+      },
+      vertical = {
+        width = 0.9,
+        height = 0.95,
+        preview_height = 0.5,
+      },
+      flex = {
+        horizontal = {
+          preview_width = 0.9,
+        },
+      },
+    },
     selection_strategy = "reset",
     sorting_strategy = "descending",
     scroll_strategy = "cycle",
