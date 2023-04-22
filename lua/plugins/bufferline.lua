ea43b6221e1ec9032f83ae9250d606c7a3e9ccdb
AngeloEnrique <angelo.zapatalillo@gmail.com>
2023-04-22 01:23:24 -0400

    [c] heirline

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -1,5 +1,6 @@
 return {
   "akinsho/nvim-bufferline.lua",
+  enabled = false,
   event = "UiEnter",
   dependencies = {
     "nvim-tree/nvim-web-devicons",
--- lua/plugins/heirline.lua
+++ lua/plugins/heirline.lua
@@ -298,7 +298,7 @@ return {
           {
             provider = " ",
             condition = function(self)
-              return self.status_dict.changed ~= 0 and (self.status_dict.removed ~= 0 or self.status_dict.added ~= 0)
+              return self.status_dict.changed ~= 0 and self.status_dict.removed ~= 0
             end,
           },
           {
@@ -352,7 +352,7 @@ return {
           {
             provider = " ",
             condition = function(self)
-              return self.status_dict.changed ~= 0 and (self.status_dict.removed ~= 0 or self.status_dict.added ~= 0)
+              return self.status_dict.changed ~= 0 and self.status_dict.removed ~= 0
             end,
           },
           {
@@ -592,270 +592,302 @@ return {
       DefaultStatusline,
     }
 
-    -- local TablineBufnr = {
-    --   provider = function(self)
-    --     return tostring(self.bufnr) .. ". "
-    --   end,
-    --   hl = "Comment",
-    -- }
-    --
-    -- -- we redefine the filename component, as we probably only want the tail and not the relative path
-    -- local TablineFileName = {
-    --   provider = function(self)
-    --     -- self.filename will be defined later, just keep looking at the example!
-    --     local filename = self.filename
-    --     filename = filename == "" and "[No Name]" or vim.fn.fnamemodify(filename, ":t")
-    --     return filename
-    --   end,
-    --   hl = function(self)
-    --     return { bold = self.is_active or self.is_visible, italic = true }
-    --   end,
-    -- }
-    --
-    -- -- this looks exactly like the FileFlags component that we saw in
-    -- -- #crash-course-part-ii-filename-and-friends, but we are indexing the bufnr explicitly
-    -- -- also, we are adding a nice icon for terminal buffers.
-    -- local TablineFileFlags = {
-    --   {
-    --     condition = function(self)
-    --       return vim.api.nvim_buf_get_option(self.bufnr, "modified")
-    --     end,
-    --     provider = "[+]",
-    --     hl = { fg = "green" },
-    --   },
-    --   {
-    --     condition = function(self)
-    --       return not vim.api.nvim_buf_get_option(self.bufnr, "modifiable")
-    --           or vim.api.nvim_buf_get_option(self.bufnr, "readonly")
-    --     end,
-    --     provider = function(self)
-    --       if vim.api.nvim_buf_get_option(self.bufnr, "buftype") == "terminal" then
-    --         return "  "
-    --       else
-    --         return ""
-    --       end
-    --     end,
-    --     hl = { fg = "orange" },
-    --   },
-    -- }
-    --
-    -- -- Here the filename block finally comes together
-    -- local TablineFileNameBlock = {
-    --   init = function(self)
-    --     self.filename = vim.api.nvim_buf_get_name(self.bufnr)
-    --   end,
-    --   hl = function(self)
-    --     if self.is_active then
-    --       return "TabLineSel"
-    --       -- why not?
-    --       -- elseif not vim.api.nvim_buf_is_loaded(self.bufnr) then
-    --       --     return { fg = "gray" }
-    --     else
-    --       return "TabLine"
-    --     end
-    --   end,
-    --   on_click = {
-    --     callback = function(_, minwid, _, button)
-    --       if (button == "m") then -- close on mouse middle click
-    --         vim.schedule(function()
-    --           vim.api.nvim_buf_delete(minwid, { force = false })
-    --         end)
-    --       else
-    --         vim.api.nvim_win_set_buf(0, minwid)
-    --       end
-    --     end,
-    --     minwid = function(self)
-    --       return self.bufnr
-    --     end,
-    --     name = "heirline_tabline_buffer_callback",
-    --   },
-    --   TablineBufnr,
-    --   FileIcon, -- turns out the version defined in #crash-course-part-ii-filename-and-friends can be reutilized as is here!
-    --   TablineFileName,
-    --   TablineFileFlags,
-    -- }
-    --
-    -- -- a nice "x" button to close the buffer
-    -- local TablineCloseButton = {
-    --   condition = function(self)
-    --     return not vim.api.nvim_buf_get_option(self.bufnr, "modified")
-    --   end,
-    --   { provider = " " },
-    --   {
-    --     provider = "",
-    --     hl = { fg = "gray" },
-    --     on_click = {
-    --       callback = function(_, minwid)
-    --         vim.schedule(function()
-    --           vim.api.nvim_buf_delete(minwid, { force = false })
-    --           vim.cmd.redrawtabline()
-    --         end)
-    --       end,
-    --       minwid = function(self)
-    --         return self.bufnr
-    --       end,
-    --       name = "heirline_tabline_close_buffer_callback",
-    --     },
-    --   },
-    -- }
-    --
-    -- -- The final touch!
-    -- local TablineBufferBlock = utils.surround({ "", "" }, function(self)
-    --   if self.is_active then
-    --     return utils.get_highlight("TabLineSel").bg
-    --   else
-    --     return utils.get_highlight("TabLine").bg
-    --   end
-    -- end, { TablineFileNameBlock, TablineCloseButton })
-    --
-    -- -- and here we go
-    -- local BufferLine = utils.make_buflist(
-    --   TablineBufferBlock,
-    --   { provider = "", hl = { fg = "gray" } }, -- left truncation, optional (defaults to "<")
-    --   { provider = "", hl = { fg = "gray" } } -- right trunctation, also optional (defaults to ...... yep, ">")
-    -- -- by the way, open a lot of buffers and try clicking them ;)
-    -- )
-    -- -- this is the default function used to retrieve buffers
-    -- local get_bufs = function()
-    --   return vim.tbl_filter(function(bufnr)
-    --     return vim.api.nvim_buf_get_option(bufnr, "buflisted")
-    --   end, vim.api.nvim_list_bufs())
-    -- end
-    --
-    -- -- initialize the buflist cache
-    -- local buflist_cache = {}
-    --
-    -- -- setup an autocmd that updates the buflist_cache every time that buffers are added/removed
-    -- vim.api.nvim_create_autocmd({ "VimEnter", "UIEnter", "BufAdd", "BufDelete" }, {
-    --   callback = function()
-    --     vim.schedule(function()
-    --       local buffers = get_bufs()
-    --       for i, v in ipairs(buffers) do
-    --         buflist_cache[i] = v
-    --       end
-    --       for i = #buffers + 1, #buflist_cache do
-    --         buflist_cache[i] = nil
-    --       end
-    --
-    --       -- check how many buffers we have and set showtabline accordingly
-    --       if #buflist_cache > 1 then
-    --         vim.o.showtabline = 2 -- always
-    --       else
-    --         vim.o.showtabline = 1 -- only when #tabpages > 1
-    --       end
-    --     end)
-    --   end,
-    -- })
-    --
-    -- local BufferLine = utils.make_buflist(
-    --   TablineBufferBlock,
-    --   { provider = " ", hl = { fg = "gray" } },
-    --   { provider = " ", hl = { fg = "gray" } },
-    --   -- out buf_func simply returns the buflist_cache
-    --   function()
-    --     return buflist_cache
-    --   end,
-    --   -- no cache, as we're handling everything ourselves
-    --   false
-    -- )
-    -- local TablinePicker = {
-    --   condition = function(self)
-    --     return self._show_picker
-    --   end,
-    --   init = function(self)
-    --     local bufname = vim.api.nvim_buf_get_name(self.bufnr)
-    --     bufname = vim.fn.fnamemodify(bufname, ":t")
-    --     local label = bufname:sub(1, 1)
-    --     local i = 2
-    --     while self._picker_labels[label] do
-    --       if i > #bufname then
-    --         break
-    --       end
-    --       label = bufname:sub(i, i)
-    --       i = i + 1
-    --     end
-    --     self._picker_labels[label] = self.bufnr
-    --     self.label = label
-    --   end,
-    --   provider = function(self)
-    --     return self.label
-    --   end,
-    --   hl = { fg = "red", bold = true },
-    -- }
-    --
-    -- vim.keymap.set("n", "gbp", function()
-    --   local tabline = require("heirline").tabline
-    --   local buflist = tabline._buflist[1]
-    --   buflist._picker_labels = {}
-    --   buflist._show_picker = true
-    --   vim.cmd.redrawtabline()
-    --   local char = vim.fn.getcharstr()
-    --   local bufnr = buflist._picker_labels[char]
-    --   if bufnr then
-    --     vim.api.nvim_win_set_buf(0, bufnr)
-    --   end
-    --   buflist._show_picker = false
-    --   vim.cmd.redrawtabline()
-    -- end)
-    -- local Tabpage = {
-    --   provider = function(self)
-    --     return "%" .. self.tabnr .. "T " .. self.tabpage .. " %T"
-    --   end,
-    --   hl = function(self)
-    --     if not self.is_active then
-    --       return "TabLine"
-    --     else
-    --       return "TabLineSel"
-    --     end
-    --   end,
-    -- }
-    --
-    -- local TabpageClose = {
-    --   provider = "%999X  %X",
-    --   hl = "TabLine",
-    -- }
-    --
-    -- local TabPages = {
-    --   -- only show this component if there's 2 or more tabpages
-    --   condition = function()
-    --     return #vim.api.nvim_list_tabpages() >= 2
-    --   end,
-    --   { provider = "%=" },
-    --   utils.make_tablist(Tabpage),
-    --   TabpageClose,
-    -- }
-    -- local TabLineOffset = {
-    --   condition = function(self)
-    --     local win = vim.api.nvim_tabpage_list_wins(0)[1]
-    --     local bufnr = vim.api.nvim_win_get_buf(win)
-    --     self.winid = win
-    --
-    --     if vim.bo[bufnr].filetype == "NvimTree" then
-    --       self.title = "NvimTree"
-    --       return true
-    --       -- elseif vim.bo[bufnr].filetype == "TagBar" then
-    --       --     ...
-    --     end
-    --   end,
-    --   provider = function(self)
-    --     local title = self.title
-    --     local width = vim.api.nvim_win_get_width(self.winid)
-    --     local pad = math.ceil((width - #title) / 2)
-    --     return string.rep(" ", pad) .. title .. string.rep(" ", pad)
-    --   end,
-    --   hl = function(self)
-    --     if vim.api.nvim_get_current_win() == self.winid then
-    --       return "TablineSel"
-    --     else
-    --       return "Tabline"
-    --     end
-    --   end,
-    -- }
-    -- local TabLine = { TabLineOffset, BufferLine, TabPages }
+    -- we redefine the filename component, as we probably only want the tail and not the relative path
+    local TablineFileName = {
+      provider = function(self)
+        -- self.filename will be defined later, just keep looking at the example!
+        local filename = self.filename
+        filename = filename == "" and "[No Name]" or vim.fn.fnamemodify(filename, ":t")
+        return filename
+      end,
+      hl = function(self)
+        return { bold = self.is_active or self.is_visible, italic = true }
+      end,
+    }
+
+    -- this looks exactly like the FileFlags component that we saw in
+    -- #crash-course-part-ii-filename-and-friends, but we are indexing the bufnr explicitly
+    -- also, we are adding a nice icon for terminal buffers.
+    local TablineFileFlags = {
+      {
+        condition = function(self)
+          return vim.api.nvim_buf_get_option(self.bufnr, "modified")
+        end,
+        provider = "[+]",
+        hl = { fg = "green" },
+      },
+      {
+        condition = function(self)
+          return not vim.api.nvim_buf_get_option(self.bufnr, "modifiable")
+              or vim.api.nvim_buf_get_option(self.bufnr, "readonly")
+        end,
+        provider = function(self)
+          if vim.api.nvim_buf_get_option(self.bufnr, "buftype") == "terminal" then
+            return "  "
+          else
+            return ""
+          end
+        end,
+        hl = { fg = "orange" },
+      },
+    }
+
+    -- Here the filename block finally comes together
+    local TablineFileNameBlock = {
+      init = function(self)
+        self.filename = vim.api.nvim_buf_get_name(self.bufnr)
+      end,
+      hl = function(self)
+        if self.is_active then
+          return {}
+          -- why not?
+        elseif not vim.api.nvim_buf_is_loaded(self.bufnr) then
+          return { fg = "gray" }
+        else
+          return "TabLine"
+        end
+      end,
+      FileIcon, -- turns out the version defined in #crash-course-part-ii-filename-and-friends can be reutilized as is here!
+      TablineFileName,
+      TablineFileFlags,
+    }
+
+
+    local TablinePicker = {
+      condition = function(self)
+        return self._show_picker
+      end,
+      init = function(self)
+        local bufname = vim.api.nvim_buf_get_name(self.bufnr)
+        bufname = vim.fn.fnamemodify(bufname, ":t")
+        local label = bufname:sub(1, 1)
+        local i = 2
+        while self._picker_labels[label] do
+          if i > #bufname then
+            break
+          end
+          label = bufname:sub(i, i)
+          i = i + 1
+        end
+        self._picker_labels[label] = self.bufnr
+        self.label = label
+      end,
+      provider = function(self)
+        return self.label
+      end,
+      hl = { fg = "red", bold = true },
+    }
+
+
+    local ViModeTab = {
+      static = {
+        mode_names = {
+          n = "N",
+          no = "N?",
+          nov = "N?",
+          noV = "N?",
+          ["no\22"] = "N?",
+          niI = "Ni",
+          niR = "Nr",
+          niV = "Nv",
+          nt = "Nt",
+          v = "V",
+          vs = "Vs",
+          V = "V_",
+          Vs = "Vs",
+          ["\22"] = "^V",
+          ["\22s"] = "^V",
+          s = "S",
+          S = "S_",
+          ["\19"] = "^S",
+          i = "I",
+          ic = "Ic",
+          ix = "Ix",
+          R = "R",
+          Rc = "Rc",
+          Rx = "Rx",
+          Rv = "Rv",
+          Rvc = "Rv",
+          Rvx = "Rv",
+          c = "C",
+          cv = "Ex",
+          r = "...",
+          rm = "M",
+          ["r?"] = "?",
+          ["!"] = "!",
+          t = "T",
+        },
+      },
+      provider = function()
+        return "│"
+      end,
+      hl = function(self)
+        if self.is_active then
+          local color = self:mode_color() -- here!
+          return { fg = color, bold = true }
+        end
+      end,
+      update = {
+        "ModeChanged",
+        pattern = "*:*",
+        callback = vim.schedule_wrap(function()
+          vim.cmd.redrawtabline()
+        end),
+      },
+    }
+
+    -- The final touch!
+    local TablineBufferBlock = {
+      hl = function(self)
+        if self.is_active then
+          return "TabLineSel"
+        else
+          return "TabLine"
+        end
+      end,
+      { ViModeTab, TablineFileNameBlock, Space, TablinePicker }
+    }
+
+    local get_bufs = function()
+      return vim.tbl_filter(function(bufnr)
+        return vim.api.nvim_buf_get_option(bufnr, "buflisted")
+      end, vim.api.nvim_list_bufs())
+    end
+
+    -- initialize the buflist cache
+    local buflist_cache = {}
+
+    -- setup an autocmd that updates the buflist_cache every time that buffers are added/removed
+    vim.api.nvim_create_autocmd({ "VimEnter", "UIEnter", "BufAdd", "BufDelete" }, {
+      callback = function()
+        vim.schedule(function()
+          local buffers = get_bufs()
+          for i, v in ipairs(buffers) do
+            buflist_cache[i] = v
+          end
+          for i = #buffers + 1, #buflist_cache do
+            buflist_cache[i] = nil
+          end
+
+          -- check how many buffers we have and set showtabline accordingly
+          if #buflist_cache > 1 then
+            vim.o.showtabline = 2 -- always
+          else
+            vim.o.showtabline = 1 -- only when #tabpages > 1
+          end
+        end)
+      end,
+    })
+
+    local BufferLine = utils.make_buflist(
+      TablineBufferBlock,
+      { provider = " ", hl = { fg = "gray" } },
+      { provider = " ", hl = { fg = "gray" } },
+      -- out buf_func simply returns the buflist_cache
+      function()
+        return buflist_cache
+      end,
+      -- no cache, as we're handling everything ourselves
+      false
+    )
+
+    vim.keymap.set("n", "<leader>b", function()
+      local tabline = require("heirline").tabline
+      local buflist = tabline._buflist[1]
+      buflist._picker_labels = {}
+      buflist._show_picker = true
+      vim.cmd.redrawtabline()
+      local char = vim.fn.getcharstr()
+      local bufnr = buflist._picker_labels[char]
+      if bufnr then
+        vim.api.nvim_win_set_buf(0, bufnr)
+      end
+      buflist._show_picker = false
+      vim.cmd.redrawtabline()
+    end)
+
+    vim.keymap.set("n", "<tab>", ":bnext<cr>", { silent = true, noremap = true })
+    vim.keymap.set("n", "<S-tab>", ":bprevious<cr>", { silent = true, noremap = true })
+
+    local Tabpage = {
+      provider = function(self)
+        return "%" .. self.tabnr .. "T " .. self.tabpage .. " %T"
+      end,
+      hl = function(self)
+        if not self.is_active then
+          return "TabLine"
+        else
+          return "TabLineSel"
+        end
+      end,
+    }
+
+    local TabPages = {
+      -- only show this component if there's 2 or more tabpages
+      condition = function()
+        return #vim.api.nvim_list_tabpages() >= 2
+      end,
+      utils.make_tablist(Tabpage),
+    }
+    local TabLineOffset = {
+      condition = function(self)
+        local win = vim.api.nvim_tabpage_list_wins(0)[2]
+        if win == nil then return false end
+        local bufnr = vim.api.nvim_win_get_buf(win)
+        self.winid = win
+
+        if vim.bo[bufnr].filetype == "NvimTree" then
+          self.title = "NvimTree"
+          return true
+          -- elseif vim.bo[bufnr].filetype == "TagBar" then
+          --     ...
+        end
+      end,
+      provider = function(self)
+        local title = self.title
+        local width = vim.api.nvim_win_get_width(self.winid)
+        local pad = math.ceil((width - #title) / 2)
+        return string.rep(" ", pad) .. title .. string.rep(" ", pad)
+      end,
+      hl = function(self)
+        if vim.api.nvim_get_current_win() == self.winid then
+          return "TabLineSel"
+        else
+          return "TabLine"
+        end
+      end,
+    }
+    local TabLine = {
+      static = {
+        mode_colors_map = {
+          n = "blue",
+          i = "green",
+          v = "cyan",
+          V = "cyan",
+          ["\22"] = "cyan",
+          c = "orange",
+          s = "purple",
+          S = "purple",
+          ["\19"] = "purple",
+          R = "orange",
+          r = "orange",
+          ["!"] = "red",
+          t = "green",
+        },
+        mode_color = function(self)
+          local mode = conditions.is_active() and vim.fn.mode() or "n"
+          return self.mode_colors_map[mode]
+        end,
+      },
+      BufferLine,
+      Align,
+      TabPages,
+      TabLineOffset
+    }
 
     require("heirline").setup {
       statusline = StatusLines,
-      -- tabline = TabLine
+      tabline = TabLine
     }
     vim.o.showtabline = 2
     vim.cmd([[au FileType * if index(['wipe', 'delete'], &bufhidden) >= 0 | set nobuflisted | endif]])
