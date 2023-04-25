8e6c5788da0fa27b1d4201a234f20abf9d68cd7c
Angelo Zapata <angelozapata@inf.udec.cl>
2023-04-25 11:33:33 -0400

    [c] testing sonarlint

---
+++ lua/plugins/sonarlint.lua
@@ -0,0 +1,22 @@
+return {
+  "https://gitlab.com/schrieveslaach/sonarlint.nvim",
+  enabled = false,
+  config = function()
+    require("sonarlint").setup {
+      server = {
+        cmd = {
+          "sonarlint-language-server",
+          -- Ensure that sonarlint-language-server uses stdio channel
+          "-stdio",
+          "-analyzers",
+          -- paths to the analyzers you need, using those for python and java in this example
+          vim.fn.expand "$MASON/share/sonarlint-analyzers/sonarjava.jar",
+        },
+      },
+      filetypes = {
+        -- Requires nvim-jdtls, otherwise an error message will be printed
+        "java",
+      },
+    }
+  end,
+}
