9c270072b11c9d9d77eae2adbbd1036be7b7c5eb
Angelo Zapata <angelozapata@inf.udec.cl>
2023-04-27 19:34:50 -0400

    [c] neo-tree

--- lua/plugins/neotree.lua
+++ lua/plugins/neotree.lua
@@ -170,11 +170,11 @@ return {
       filesystem = {
         filtered_items = {
           visible = false, -- when true, they will just be displayed differently than normal items
-          hide_dotfiles = true,
-          hide_gitignored = true,
-          hide_hidden = true, -- only works on Windows for hidden files/directories
+          hide_dotfiles = false,
+          hide_gitignored = false,
+          hide_hidden = false, -- only works on Windows for hidden files/directories
           hide_by_name = {
-            --"node_modules"
+            -- "node_modules"
           },
           hide_by_pattern = { -- uses glob style patterns
             --"*.meta",
