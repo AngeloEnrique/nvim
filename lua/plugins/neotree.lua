7aa2390c46686275896d2c72fb4f5a67036d0cd4
Angelo Zapata <angelozapata@inf.udec.cl>
2023-09-20 17:03:12 -0300

    [c] neotree.

--- lua/plugins/neotree.lua
+++ lua/plugins/neotree.lua
@@ -168,12 +168,12 @@ return {
       nesting_rules = {},
       filesystem = {
         filtered_items = {
-          visible = false, -- when true, they will just be displayed differently than normal items
+          visible = true, -- when true, they will just be displayed differently than normal items
           hide_dotfiles = false,
-          hide_gitignored = false,
+          hide_gitignored = true,
           hide_hidden = false, -- only works on Windows for hidden files/directories
           hide_by_name = {
-            -- "node_modules"
+            "node_modules"
           },
           hide_by_pattern = { -- uses glob style patterns
             --"*.meta",
