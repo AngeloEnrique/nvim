5c15414a5b7df5297a1acc4ac7b5b8d0743d4048
Angelo Zapata <angelozapata@inf.udec.cl>
2024-04-08 09:50:06 -0400

    feat: change go keybindings

--- lua/plugins/go.lua
+++ lua/plugins/go.lua
@@ -4,9 +4,9 @@ return {
   config = function()
     local gopher = require "gopher"
     gopher.setup {}
-    vim.keymap.set("n", "<leader>gsj", "<cmd>GoTagAdd json<CR>", { noremap = true, silent = true })
-    vim.keymap.set("n", "<leader>gsd", "<cmd>GoTagAdd db<CR>", { noremap = true, silent = true })
-    vim.keymap.set("n", "<leader>gsy", "<cmd>GoTagAdd yaml<CR>", { noremap = true, silent = true })
+    vim.keymap.set("n", "<leader>csj", "<cmd>GoTagAdd json<CR>", { noremap = true, silent = true })
+    vim.keymap.set("n", "<leader>csd", "<cmd>GoTagAdd db<CR>", { noremap = true, silent = true })
+    vim.keymap.set("n", "<leader>csy", "<cmd>GoTagAdd yaml<CR>", { noremap = true, silent = true })
   end,
   build = function()
     vim.cmd [[silent! GoInstallDeps]]
