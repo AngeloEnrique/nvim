de22d96420f3237f363e816dbbbfbd8f26bcba3b
Angelo Zapata <angelozapata@inf.udec.cl>
2024-09-06 18:42:08 -0400

    feat: render-markdown minor changes

--- lua/plugins/render-markdown.lua
+++ lua/plugins/render-markdown.lua
@@ -1,6 +1,12 @@
 return {
   "MeanderingProgrammer/render-markdown.nvim",
   ft = { "markdown" },
-  opts = {},
+  opts = {
+    win_options = {
+      conceallevel = {
+        default = 0,
+      },
+    },
+  },
   dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-tree/nvim-web-devicons" },
 }
